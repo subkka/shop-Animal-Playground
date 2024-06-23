a263409df76e2efabf6b552ad59ecc0425ea0921
subkka <beeniaa@naver.com>
2024-06-24 08:44:11 +0900

    Feat: 환불 상세 조회, 처리 상태 업데이트, 환불된 상품 추가 쿼리 생성 #14 #15

--- admin_page/src/main/java/com/playground/admin_page/main/refund/controller/RefundController.java
+++ admin_page/src/main/java/com/playground/admin_page/main/refund/controller/RefundController.java
@@ -6,7 +6,8 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import java.util.List;
 
@@ -23,5 +24,22 @@ public class RefundController {
         log.debug("refundList = {}", refundList);
         model.addAttribute("refundList", refundList);
         return "refundList";
+    }
+
+    @GetMapping("/refundDetailList/{orderId}")
+    public String refundDetailList(@PathVariable("orderId") Long id, Model model) {
+        log.info("GET refundDetailList");
+        RefundDto refundDetailList = refundService.findRefundDetailList(id);
+        log.debug("refundDetailList = {}", refundDetailList);
+        model.addAttribute("refundDetailList", refundDetailList);
+        return "refundDetailList";
+    }
+
+    @PostMapping("/updateProcessStatus/{orderId}")
+    public String updateProcessStatus(@PathVariable("orderId") Long id, @RequestParam String processStatus, RedirectAttributes redirectAttributes) {
+        log.info("POST updateProcessStatus");
+        int refundDetailList = refundService.updateProcessStatus(id, processStatus);
+        redirectAttributes.addFlashAttribute("updateResult", refundDetailList > 0 ? "처리 상태가 변경되었습니다." : "처리 상태 변경을 실패했습니다. 다시 시도해주세요.");
+        return "redirect:/refundDetailList";
     }
 }
--- admin_page/src/main/java/com/playground/admin_page/main/refund/model/dao/RefundMapper.java
+++ admin_page/src/main/java/com/playground/admin_page/main/refund/model/dao/RefundMapper.java
@@ -1,11 +1,21 @@
 package com.playground.admin_page.main.refund.model.dao;
 
 import com.playground.admin_page.main.refund.model.dto.RefundDto;
+import com.playground.admin_page.main.refund.model.dto.RefundProductDto;
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
 
 import java.util.List;
 
 @Mapper
 public interface RefundMapper {
     List<RefundDto> findRefundList();
+
+    RefundDto findRefundDetailList(Long id);
+
+    int updateProcessStatus(@Param("orderId") Long id, @Param("processStatus") String processStatus);
+
+    int insertRefundProduct(List<RefundProductDto> refundProductList);
+
+    List<RefundProductDto> findProductForRefund(Long id);
 }
