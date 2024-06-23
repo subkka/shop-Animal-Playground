ed6a9431af08c9554de71d8829c2cb228e19fb4d
ParkMinhyeok <parkminem@gmail.com>
2024-06-23 21:53:04 +0900

    Feat: 상품 조회 프론트 완료

--- admin_page/src/main/java/com/playground/admin_page/main/product/controller/ProductController.java
+++ admin_page/src/main/java/com/playground/admin_page/main/product/controller/ProductController.java
@@ -1,4 +1,27 @@
 package com.playground.admin_page.main.product.controller;
 
+import com.playground.admin_page.main.product.dto.ProductDto;
+import com.playground.admin_page.main.product.service.ProductService;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
+
+@Controller
+@Slf4j
+@RequestMapping("/product")
+@RequiredArgsConstructor
 public class ProductController {
+    private final ProductService productService;
+    @GetMapping("/list")
+    public void list(Model model) {
+        log.info("GET /product/list");
+        List<ProductDto> products = productService.findAll();
+        log.debug("products ={}", products);
+        model.addAttribute("products", products);
+    }
 }
