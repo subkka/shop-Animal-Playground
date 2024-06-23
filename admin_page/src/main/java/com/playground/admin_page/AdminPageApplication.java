a7365113969bb5713e65d2e2ee19b448d19d07e4
subkka <beeniaa@naver.com>
2024-06-23 14:46:29 +0900

    Update: 오타 수정 및 @MapperScan 추가

--- admin_page/src/main/java/com/playground/admin_page/AdminPageApplication.java
+++ admin_page/src/main/java/com/playground/admin_page/AdminPageApplication.java
@@ -1,9 +1,11 @@
 package com.playground.admin_page;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@MapperScan
 public class AdminPageApplication {
 
     public static void main(String[] args) {
