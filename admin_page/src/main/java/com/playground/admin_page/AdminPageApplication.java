c4e322f52979afac63ada9572f3f15456ba60331
hurryduck <tigeryck0812@gmail.com>
2024-06-25 17:33:06 +0900

    update: 미로그인시 다른 사이트 접속 제한 AOP -> Filter로 로직 변경 #20

--- admin_page/src/main/java/com/playground/admin_page/AdminPageApplication.java
+++ admin_page/src/main/java/com/playground/admin_page/AdminPageApplication.java
@@ -4,9 +4,11 @@ package com.playground.admin_page;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletComponentScan;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @SpringBootApplication
+@ServletComponentScan
 @MapperScan
 public class AdminPageApplication {
 
