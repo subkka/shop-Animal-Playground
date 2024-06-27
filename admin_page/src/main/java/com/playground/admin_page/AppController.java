d2099fabc1858464f1a461df64574b14d9efb3cd
hurryduck <tigeryck0812@gmail.com>
2024-06-27 11:54:57 +0900

    update: 로그인 오류 해결

--- admin_page/src/main/java/com/playground/admin_page/AppController.java
+++ admin_page/src/main/java/com/playground/admin_page/AppController.java
@@ -1,10 +1,22 @@
 package com.playground.admin_page;
 
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpSession;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
+@Slf4j
 public class AppController {
+    @GetMapping("")
+    public String index(HttpServletRequest request) {
+        log.info("GET /index");
+        HttpSession session = request.getSession(); // 비동기를 위한 Session 객체 생성
+        return "index";
+    }
+
+
     @GetMapping("/main")
     public String getMain() {
         return "main";
