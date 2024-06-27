a2426304086ac86037e60db358a343ebb133ab07
hurryduck <tigeryck0812@gmail.com>
2024-06-27 12:10:06 +0900

    update: 코드 정리

--- admin_page/src/main/java/com/playground/admin_page/AppController.java
+++ admin_page/src/main/java/com/playground/admin_page/AppController.java
@@ -1,14 +1,22 @@
 package com.playground.admin_page;
 
+import com.playground.admin_page.main.order.service.OrderService;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpSession;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.List;
 
 @Controller
 @Slf4j
+@RequiredArgsConstructor
 public class AppController {
+    private final OrderService orderService;
+
     @GetMapping("")
     public String index(HttpServletRequest request) {
         log.info("GET /index");
@@ -17,7 +25,11 @@ public class AppController {
     }
 
     @GetMapping("/main")
-    public String getMain() {
+    public String getMain(Model model) {
+        List<String> getKindStatus = orderService.getKindStaus();
+        List<Integer> getCountStatus = orderService.getCountStatus();
+        model.addAttribute("getKindStatus", getKindStatus);
+        model.addAttribute("getCountStatus", getCountStatus);
         return "main";
     }
 
